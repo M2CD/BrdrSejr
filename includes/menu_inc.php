6222a55b5209b367d609937019746487ba62a4aa
ColonelFluff <gunnyarts@gmail.com>
2014-12-08 13:08:01 +0100

    added basic structure and started header styling

--- includes/menu_inc.php
+++ includes/menu_inc.php
@@ -1,6 +1,10 @@
 <nav class="menu-main">
 	<ul>
-		<li><a href="#">Link 1</a></li>
-		<li><a href="#">Link 1</a></li>
+		<li><a href="#">Forside</a></li>
+		<li><a href="#">Brdr. Sejr</a></li>
+		<li><a href="#">Motorcykler</a></li>
+		<li><a href="#">Scooter/ATV/EasyGo</a></li>
+		<li><a href="#">Værksted</a></li>
+		<li><a href="#">Kontakt</a></li>
 	</ul>
 </nav>
--- index.php
+++ index.php
@@ -2,20 +2,66 @@
 <html>
 <head>
 	<title>Brdr. Sejr Motorcykler ApS</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href='http://fonts.googleapis.com/css?family=Fira+Sans:300,400,500,700,300italic,400italic,500italic,700italic|Alegreya+Sans+SC:500,700' rel='stylesheet' type='text/css'>
 	<link href="css/reset.css" type="text/css" rel="stylesheet">
 	<link href="css/style.css" type="text/css" rel="stylesheet">
 </head>
 <body>
+	<header>
+		<a href="#" title="Brdr. Sejr Motorcykler ApS" class="logo"><img src="img/logo.png" alt="Brdr. Sejr Motorcykler ApS"></a>
+		<a href="#" class="webshop">Køb Udstyr</a>
+		<?php include 'includes/menu_inc.php'; ?>
+	</header>
+	<section class="slider">
+	</section>
 	<main>
-		<header>
-			<?php include 'includes/menu_inc.php'; ?>
-		</header>
-		<section>
-			
-		</section>
-		<footer>
-		</footer>
+		<nav>
+			<a href="#" title="Se vores store udvalg af motorcykler">Motorcykler</a>
+			<a href="#" title="Se info omkring vores værksted">Værksted</a>
+			<a href="#" title="Køb dit udstyr her">Udstyr-shop</a>
+		</nav>
+		<h2>Velkommen til Brdr. Sejr</h2>
+		<p>Vi står klar til at fortælle meget mere om dit næste køretøj, kom forbi og sæt dig til rette i netop din favorit-model og få en prøvetur.</p>
+		<p>Kom ind og se vore store udvalg af SUZUKI og HONDA motercykler samt PGO scootere</p>
+		<p><strong>Hjælpen er lige om hjørnet</strong></p>
+		<aside class="trustpilot">
+			<a href="#">Anmeld os på TrustPilot</a>
+		</aside>
+		<aside class="facebook">
+			<div id="fb-root"></div>
+			<script>
+				(function(d, s, id) {
+					var js, fjs = d.getElementsByTagName(s)[0];
+					if (d.getElementById(id)) return;
+					js = d.createElement(s); js.id = id;
+					js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&appId=30274178883&version=v2.0";
+					fjs.parentNode.insertBefore(js, fjs);
+				}
+				(document, 'script', 'facebook-jssdk'));
+			</script>
+		</aside>
+		<hr>
+		<h2>Vi forhandler</h2>
+		<nav class="menu-brands">
+			<a href="#"><img src="img/brands/suzuki.png" alt="Suzuki logo"></a>
+			<a href="#"><img src="img/brands/honda.png" alt="Honda logo"></a>
+			<a href="#"><img src="img/brands/pgo.png" alt="PGO logo"></a>
+			<a href="#"><img src="img/brands/kymco.png" alt="Kymco logo"></a>
+		</nav>
+		<hr>
+		<blockquote class="reviews">
+			<h2>Hvad siger kunderne?</h2>
+			<p><cite>"Virkelig et sted jeg kan anbefale til andre, både køb af tøj og mc. De har også et kanon værksted der har meget at se til, men de holder deres aftaler! Og er gode til at rådgive og billige forhold til mange andre. Engang sejer, altid sejer:)"</cite><br><strong>- Marius Jensen Lyseggen</strong></p>
+		</blockquote>
 	</main>
+	<footer>
+		<address>Bøgegade 3 - Randersvej 36, 8200 Aarhus N, DK • Telefonnr.: 8616 3766 - 8616 3396 • E-mail: brdrsejr@brdrsejr.dk • CVR-nummer: 69955711</address>
+		<p class="open-hours store">Butikken har åbent alle dage 10:00 til 17:00</p>
+		<p class="open-hours repairs">Værkstedet hr åbent alle hverdage 9:00 til 17:00</p>
+		<p class="copyright">Copyright &copy; Brdr. Sejr Motorcykler ApS 2014</p>
+	</footer>
 	
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 	<script src="js/script.js" type="text/javascript"></script>
