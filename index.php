243fb9d83952e0c2201bbc02dfd016455b206e77
ColonelFluff <gunnyarts@gmail.com>
2014-12-09 12:11:15 +0100

    styled welcome article

--- index.php
+++ index.php
@@ -11,35 +11,34 @@
 			<a href="#" title="Se info omkring vores værksted" class="rep">Værksted</a>
 			<a href="#" title="Køb dit udstyr her" class="acc">Udstyr-shop</a>
 		</nav>
-		<h2>Velkommen til Brdr. Sejr</h2>
-		<p>Vi står klar til at fortælle meget mere om dit næste køretøj, kom forbi og sæt dig til rette i netop din favorit-model og få en prøvetur.</p>
-		<p>Kom ind og se vore store udvalg af SUZUKI og HONDA motercykler samt PGO scootere</p>
-		<p><strong>Hjælpen er lige om hjørnet</strong></p>
-		<aside class="trustpilot">
-			<a href="#">Anmeld os på TrustPilot</a>
-		</aside>
-		<aside class="facebook">
-			<div id="fb-root"></div>
-			<script>
-				(function(d, s, id) {
-					var js, fjs = d.getElementsByTagName(s)[0];
-					if (d.getElementById(id)) return;
-					js = d.createElement(s); js.id = id;
-					js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&appId=30274178883&version=v2.0";
-					fjs.parentNode.insertBefore(js, fjs);
-				}
-				(document, 'script', 'facebook-jssdk'));
-			</script>
-		</aside>
-		<hr>
-		<h2>Vi forhandler</h2>
-		<nav class="menu-brands">
-			<a href="#"><img src="img/brands/suzuki.png" alt="Suzuki logo"></a>
-			<a href="#"><img src="img/brands/honda.png" alt="Honda logo"></a>
-			<a href="#"><img src="img/brands/pgo.png" alt="PGO logo"></a>
-			<a href="#"><img src="img/brands/kymco.png" alt="Kymco logo"></a>
-		</nav>
-		<hr>
+		<article class="welcome">
+			<h2>Velkommen til Brdr. Sejr</h2>
+			<p>Vi står klar til at fortælle meget mere om dit næste køretøj, kom forbi og sæt dig til rette i netop din favorit-model og få en prøvetur.</p>
+			<p>Kom ind og se vore store udvalg SUZUKI og HONDA motercykler samt PGO scootere</p>
+			<p><strong>Hjælpen er lige om hjørnet</strong></p>
+		
+			<aside class="trustpilot">
+				<a href="#">Anmeld os på TrustPilot</a>
+			</aside>
+			<aside
+				 class="fb-like-box"
+				 data-href="https://www.facebook.com/pages/BrdrSejr-Motorcykler-ApS/176834855685134"
+				 data-colorscheme="light"
+				 data-show-faces="false"
+				 data-header="false"
+				 data-stream="false"
+				 data-show-border="false">
+			</aside>
+		</article>
+		<article class="brands">
+			<h2>Vi forhandler</h2>
+			<nav class="menu-brands">
+				<a href="#"><img src="img/brands/suzuki.png" alt="Suzuki logo"></a>
+				<a href="#"><img src="img/brands/honda.png" alt="Honda logo"></a>
+				<a href="#"><img src="img/brands/pgo.png" alt="PGO logo"></a>
+				<a href="#"><img src="img/brands/kymco.png" alt="Kymco logo"></a>
+			</nav>
+		</article>
 		<blockquote class="reviews">
 			<h2>Hvad siger kunderne?</h2>
 			<p><cite>"Virkelig et sted jeg kan anbefale til andre, både køb af tøj og mc. De har også et kanon værksted der har meget at se til, men de holder deres aftaler! Og er gode til at rådgive og billige forhold til mange andre. Engang sejer, altid sejer:)"</cite><br><strong>- Marius Jensen Lyseggen</strong></p>
@@ -51,7 +50,17 @@
 		<p class="open-hours repairs">Værkstedet hr åbent alle hverdage 9:00 til 17:00</p>
 		<p class="copyright">Copyright &copy; Brdr. Sejr Motorcykler ApS 2014</p>
 	</footer>
-	
+	<div id="fb-root"></div>
+	<script>
+		(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&appId=30274178883&version=v2.0";
+			fjs.parentNode.insertBefore(js, fjs);
+		}
+		(document, 'script', 'facebook-jssdk'));
+	</script>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 	<script src="js/script.js" type="text/javascript"></script>
 </body>
